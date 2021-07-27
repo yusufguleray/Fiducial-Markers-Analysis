509e686fefb6544393bd0a3bcb059cc8e672dd6f
yus <yusufziya95@hotmail.com>
2021-07-27 21:28:40 +0200

    Charuco update and testing capabilities

--- src/ar_allTags.py
+++ src/ar_allTags.py
@@ -7,15 +7,14 @@ import pyStag as stag
 import utils
 import time  #For calculating the time it takes to calculate
 
-# Defines the path of the calibration file and the dictonary used
-calibration_path = "../calibration/calibfiles/realsense_d415_010721_2.npz"
+isApril = False
+isAruCo = False
+isCharuco = True
+isStag = False
 
-# Load calibration from file
-mtx = None
-dist = None
-with np.load(calibration_path) as X:
-	mtx, dist, _, _ = [X[i] for i in ('mtx', 'dist', 'rvecs', 'tvecs')]
+isTest = False
 
+mtx, dist = utils.getCalibData("realsense_d415_010721_2.npz")
 
 # Initialize communication with intel realsense
 pipeline = rs.pipeline()
@@ -32,124 +31,118 @@ except:
 
 print("Connection is succesful!")
 
-
 arUcoDictionary = aruco.DICT_6X6_250
 charUcoDictionary = aruco.DICT_6X6_250
 # Define what the calibration board looks like (same as the pdf)
 charUcoBoard = cv2.aruco.CharucoBoard_create(4, 4, .045, .0225, aruco.Dictionary_get(charUcoDictionary))
 
 print("Press [ESC] to close the application")
+
+"--- Data for testing ---"
+nOfFrames = 0
+averageCalculationTime = 0
+averageDetectionNumber = 0
+previousTags = None
+
 while True:
+	time_start = time.perf_counter()
 
 	# Get frame from realsense and convert to grayscale image
 	frames = pipeline.wait_for_frames()
 	img_rgb = np.asanyarray(frames.get_color_frame().get_data())
 	img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
 	
-	" --- AprilTag --- "
-	# Detect markers on the gray image
-	aprilDetector = apriltag("tagStandard41h12")
-	detections = aprilDetector.detect(img_gray)
+	if isApril == True:
+		" --- AprilTag --- "
+		# Detect markers on the gray image
+		aprilDetector = apriltag("tagStandard41h12")
+		detections = aprilDetector.detect(img_gray)
+		nOfDetections = len(detections)
+			
+		# Draw each marker 
+		for tag in detections:
+			corners2 = tag["lb-rb-rt-lt"]
+			corners2 = np.array([corners2[::-1]])
+			# Estimate pose of the respective marker, with matrix size 1x1
+			rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners2, 1, mtx, dist)
+
+			img_rgb = utils.drawCube(img_rgb, rvecs, tvecs, mtx, dist)
+
+		" --- End of AprilTag --- "
+
+	if isAruCo == True:
+		" --- ArUco --- "
+		corners, ids, rejectedImgPoints = aruco.detectMarkers(img_gray, aruco.getPredefinedDictionary(arUcoDictionary))
 		
-	# Draw each marker 
-	for tag in detections:
-		corners2 = tag["lb-rb-rt-lt"]
-		corners2 = np.array([corners2[::-1]])
-		# Estimate pose of the respective marker, with matrix size 1x1
-		rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners2, 1, mtx, dist)
+		nOfDetections = len(corners)
+		# Draw each marker 
+		for i in range(len(corners)):
+			# Estimate pose of the respective marker, with matrix size 1x1
+			rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners[i], 1, mtx, dist)
+			img_rgb = utils.drawCube(img_rgb, rvecs, tvecs, mtx, dist, (0, 255, 0))
 
-		print('April shape of rvec is' ,rvecs, '\n April shape of tvec is', tvecs)
-		img_rgb = utils.drawCube(img_rgb, rvecs, tvecs, mtx, dist)
-		aruco.drawAxis(img_rgb, mtx, dist, rvecs, tvecs, 0.1)
+		" --- End of AruCo --- "
 
-	" --- End of AprilTag --- "
+	if isCharuco == True:
+		" --- CharUco --- "
+		corners, ids, rejected_points = cv2.aruco.detectMarkers(img_gray, aruco.getPredefinedDictionary(charUcoDictionary))
+		print(ids)
+		if (corners is not None and ids is not None) and (len(corners) == len(ids) and len(corners) != 0):
+			# try:
+			ret1, c_corners, c_ids = cv2.aruco.interpolateCornersCharuco(corners, ids, img_gray, charUcoBoard)
 
-	" --- ArUco --- "
-	corners, ids, rejectedImgPoints = aruco.detectMarkers(img_gray, aruco.getPredefinedDictionary(arUcoDictionary))
-	
-	# Draw each marker 
-	for i in range(len(corners)):
-		# Estimate pose of the respective marker, with matrix size 1x1
-		rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners[i], 1, mtx, dist)
-		img_rgb = utils.drawCube(img_rgb, rvecs, tvecs, mtx, dist, (0, 255, 0))
+			rvecN = None 
+			tvecN = None
+			ret2, p_rvec, p_tvec = cv2.aruco.estimatePoseCharucoBoard(c_corners, c_ids, charUcoBoard, mtx, dist, rvecN, tvecN, useExtrinsicGuess=False)
+			if (p_rvec is not None and p_tvec is not None) and (not np.isnan(p_rvec).any() and not np.isnan(p_tvec).any()) :
+				R , _ = cv2.Rodrigues(p_rvec)
+				# p_tvec = p_tvec + ( R @ np.array([0, 0.09 , -0.09])).reshape(3,1)
+				img_rgb = utils.drawCubeChar(img_rgb, p_rvec, p_tvec*5.53, mtx, dist, (255, 255, 255))
+				# aruco.drawAxis(img_rgb, mtx, dist, p_rvec, p_tvec, 1)
+				
 
-	" --- End of AruCo --- "
+			# The problem is that charuco translation vector is too close
+		" --- End of CharUco --- "
 
-	" --- CharUco --- "
-	corners, ids, rejected_points = cv2.aruco.detectMarkers(img_gray, aruco.getPredefinedDictionary(charUcoDictionary))
+	if isStag == True:
+		" --- STag --- "
+		stagDetector = stag.Detector(11, 7, False)
+		numberofMarkers = stagDetector.detect(img_gray)
+		markerIDs = stagDetector.getIds()
+		detections = stagDetector.getContours()
 
-	if (corners is not None and ids is not None) and (len(corners) == len(ids) and len(corners) != 0):
-		print('Passed the 1st')
-		# try:
-		ret1, c_corners, c_ids = cv2.aruco.interpolateCornersCharuco(corners,
-																	ids,
-																	img_gray,
-																	charUcoBoard)
+		nOfDetections = len(detections)
 
-		rvecN = None 
-		tvecN = None
-		ret2, p_rvec, p_tvec = cv2.aruco.estimatePoseCharucoBoard(c_corners,
-																c_ids,
-																charUcoBoard,
-																mtx,
-																dist,
-																rvecN, tvecN, useExtrinsicGuess=False)
-		print(ret1,ret2)
-		if (p_rvec is not None and p_tvec is not None) and (not np.isnan(p_rvec).any() and not np.isnan(p_tvec).any()) :
-			print("Passed the 2nd", p_rvec, p_tvec)
-			print('Char shape of rvec is' ,rvecs, '\n Char Shape of tvec is', tvecs)
-			# img_rgb = utils.drawCube(img_rgb, p_rvec, p_tvec, mtx, dist, (255, 255, 255))
-			aruco.drawAxis(img_rgb, mtx, dist, p_rvec, p_tvec/10, 0.1)
-        # cv2.aruco.drawDetectedCornersCharuco(frame, c_corners, c_ids)
-        # cv2.aruco.drawDetectedMarkers(frame, corners, ids)
-        # cv2.aruco.drawDetectedMarkers(frame, rejected_points, borderColor=(100, 0, 240))
-
-		# except cv2.error:
-		# 	pass
-        
-
-	
-	# if len(corners) == 8:
-	# 	retval, charucoCorners, charucoIds = aruco.interpolateCornersCharuco(corners, ids, img_gray, charUcoBoard)
+		for tag in detections:
+			corners2 = tag
+			corners2 = np.array([corners2[::1]])
+			# Estimate pose of the respective marker, with matrix size 1x1
+			rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners2, 1, mtx, dist)
 		
-	# 	# Draw each marker 
-	# 	for i in range(int(len(charucoIds)/9)):
-	# 		# Estimate pose of the respective marker, with matrix size 1x1
-	# 		rvecN = np.zeros((3,3))
-	# 		rvecN = np.array([rvecN])
-	# 		tvecN = np.zeros(3)
-	# 		tvecN = np.array([[tvecN]])
-	# 		retval, rvec, tvec = aruco.estimatePoseCharucoBoard(charucoCorners, charucoIds, charUcoBoard, mtx, dist, rvecN, tvecN, useExtrinsicGuess=False)
-	# 		rvec = rvec.reshape(1,1,3)
-	# 		tvec = tvec.reshape(1,1,3)
-
-	# 		img_rgb = utils.drawCube(img_rgb, rvec, tvec, mtx, dist)
-	# 		aruco.drawAxis(img_rgb, mtx, dist, rvec, tvec, 0.1)
-
-	" --- End of CharUco --- "
-
-	" --- STag --- "
-	stagDetector = stag.Detector(11, 7, False)
-	numberofMarkers = stagDetector.detect(img_gray)
-	markerIDs = stagDetector.getIds()
-	detections = stagDetector.getContours()
-
-	for tag in detections:
-		corners2 = tag
-		corners2 = np.array([corners2[::1]])
-		# Estimate pose of the respective marker, with matrix size 1x1
-		rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners2, 1, mtx, dist)
-	
-		img_rgb = utils.drawCube(img_rgb, rvecs, tvecs, mtx, dist, (0,0,255))
-		
-	" --- End of Stag --- "
+			img_rgb = utils.drawCube(img_rgb, rvecs, tvecs, mtx, dist, (0,0,255))
+			
+		" --- End of Stag --- "
 
 	# Display the result
 	cv2.imshow("AR-Example", cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR))
 	
+	if isTest == True:
+		"--- Testing Stuff ---"
+		nOfFrames+=1
+		time_elapsed = (time.perf_counter() - time_start)
+		averageCalculationTime = averageCalculationTime + 1/nOfFrames*(time_elapsed - averageCalculationTime)
+		averageDetectionNumber = averageDetectionNumber + 1/nOfFrames*(nOfDetections - averageDetectionNumber)
+		positional_error, orrientational_error  = utils.pose_error()
+
+
 	# If [ESC] pressed, close the application
 	if cv2.waitKey(100) == 27:
 		print("Application closed")
+		if isTest == True:
+			print("   ---   The results of the test   ---")
+			print("Number of the frames is : ", nOfFrames)
+			print("Average computation time is : ", averageCalculationTime, "seconds")
+			print("Average number of detections is : ", averageDetectionNumber)
 		break
 # Close all cv2 windows
 cv2.destroyAllWindows()
--- src/utils.py
+++ src/utils.py
@@ -58,6 +58,63 @@ def drawCube(img_rgb, rvecs, tvecs, mtx, dist, cubColor=(255, 0, 0)):
 
 	return img_rgb
 
+def drawCubeChar(img_rgb, rvecs, tvecs, mtx, dist, cubColor=(255, 0, 0)):
+	# Define the ar cube
+	# Since we previously set a matrix size of 1x1 for the marker and we want the cube to be the same size, it is also defined with a size of 1x1x1
+	# It is important to note that the center of the marker corresponds to the origin and we must therefore move 0.5 away from the origin 
+	axis = np.float32([[0, 0, 0], [0, 1, 0], [1, 1, 0], [1, 0, 0],
+						[0, 0, 1], [0, 1, 1], [1, 1, 1],[1, 0, 1]])
+	# Now we transform the cube to the marker position and project the resulting points into 2d
+	imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
+	imgpts = np.int32(imgpts).reshape(-1, 2)
+
+	# Now comes the drawing. 
+	# In this example, I would like to draw the cube so that the walls also get a painted
+	# First create six copies of the original picture (for each side of the cube one)
+	side1 = img_rgb.copy()
+	side2 = img_rgb.copy()
+	side3 = img_rgb.copy()
+	side4 = img_rgb.copy()
+	side5 = img_rgb.copy()
+	side6 = img_rgb.copy()
+	
+	# Draw the bottom side (over the marker)
+	side1 = cv2.drawContours(side1, [imgpts[:4]], -1, cubColor, -2)
+	# Draw the top side (opposite of the marker)
+	side2 = cv2.drawContours(side2, [imgpts[4:]], -1, cubColor, -2)
+	# Draw the right side vertical to the marker
+	side3 = cv2.drawContours(side3, [np.array(
+		[imgpts[0], imgpts[1], imgpts[5],
+			imgpts[4]])], -1, cubColor, -2)
+	# Draw the left side vertical to the marker
+	side4 = cv2.drawContours(side4, [np.array(
+		[imgpts[2], imgpts[3], imgpts[7],
+			imgpts[6]])], -1, cubColor, -2)
+	# Draw the front side vertical to the marker
+	side5 = cv2.drawContours(side5, [np.array(
+		[imgpts[1], imgpts[2], imgpts[6],
+			imgpts[5]])], -1, cubColor, -2)
+	# Draw the back side vertical to the marker
+	side6 = cv2.drawContours(side6, [np.array(
+		[imgpts[0], imgpts[3], imgpts[7],
+			imgpts[4]])], -1, cubColor, -2)
+	
+	# Until here the walls of the cube are drawn in and can be merged
+	img_rgb = cv2.addWeighted(side1, 0.1, img_rgb, 0.9, 0)
+	img_rgb = cv2.addWeighted(side2, 0.1, img_rgb, 0.9, 0)
+	img_rgb = cv2.addWeighted(side3, 0.1, img_rgb, 0.9, 0)
+	img_rgb = cv2.addWeighted(side4, 0.1, img_rgb, 0.9, 0)
+	img_rgb = cv2.addWeighted(side5, 0.1, img_rgb, 0.9, 0)
+	img_rgb = cv2.addWeighted(side6, 0.1, img_rgb, 0.9, 0)
+	
+	# Now the edges of the cube are drawn thicker and stronger
+	img_rgb = cv2.drawContours(img_rgb, [imgpts[:4]], -1, cubColor, 2)
+	for i, j in zip(range(4), range(4, 8)):
+		img_rgb = cv2.line(img_rgb, tuple(imgpts[i]), tuple(imgpts[j]), cubColor, 2)
+	img_rgb = cv2.drawContours(img_rgb, [imgpts[4:]], -1, cubColor, 2)
+
+	return img_rgb
+
 def calibrator():
 	import cv2
 	import cv2.aruco as aruco
@@ -139,6 +196,13 @@ def calibrator():
 			cv2.destroyAllWindows()
 			break
 
+def getCalibPath(filename):
+	import os
+
+	# Defines the path of the calibration file and the dictonary used
+	return os.path.join(os.path.split(os.path.dirname(__file__))[0], 'calibration','calibfiles', filename)
+
+
 def displayCalibMat(filename, showmtx= True, showDist=True):
 	import os
 
@@ -153,3 +217,24 @@ def displayCalibMat(filename, showmtx= True, showDist=True):
 
 	if showmtx==True:print('Calibration Matrix : \n' , mtx,'\n')
 	if showDist==True:print('Distortion Parameters : \n' ,dist,'\n')
+
+def getCalibData(filename):
+	import os
+
+	# Defines the path of the calibration file and the dictonary used
+	calibration_path = os.path.join(os.path.split(os.path.dirname(__file__))[0], 'calibration','calibfiles', filename)
+
+	# Load calibration from file
+	mtx = None
+	dist = None
+	with np.load(calibration_path) as X:
+		mtx, dist, _, _ = [X[i] for i in ('mtx', 'dist', 'rvecs', 'tvecs')]
+
+	return mtx, dist
+
+def angle_error(v1, v2):
+	# v1 is your firsr vector
+	# v2 is your second vector
+    return np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
+
+# def pose_error(prevPose, curPose):
